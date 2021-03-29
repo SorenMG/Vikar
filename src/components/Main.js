fe024106bafc47cda70bc239adaa24ea3fb6617f
Søren Møller Gade Hansen <soren@Srens-MacBook-Pro.local>
2021-03-29 15:04:18 +0200

    add row to recently added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, ListItem, List, View, Thumbnail, Icon } from 'native-base';
+import { Text, ListItem, List, View, Thumbnail, Icon, Right } from 'native-base';
 import { Container, Error, Loading } from './UI';
 import { errorMessages } from '../constants/messages';
 import color from '../constants/color';
@@ -98,29 +98,77 @@ const JobPosting = ({ error, loading }) => {
           <Text style={{ fontFamily: 'Gilroy', fontSize: 18, marginVertical: 8 }}>
             Produktionsarbejde
           </Text>
-          <Text style={{ fontFamily: 'GilroyLight', fontSize: 18 }}>50 DKK/t</Text>
+          <Text style={{ fontFamily: 'GilroyLight', fontSize: 18 }}>dkk 50/t</Text>
         </View>
       </View>
     );
   };
 
+  const ItemDivider = ({ title }) => (
+    <ListItem
+      itemHeader
+      first
+      itemDivider
+      style={{
+        paddingBottom: spacing.interPadding,
+        paddingTop: spacing.interPadding + 8,
+        paddingLeft: 0,
+        backgroundColor: 'transparent',
+      }}
+    >
+      <Text style={{ fontFamily: 'Gilroy', fontSize: 17 }}>{title}</Text>
+    </ListItem>
+  );
+
+  const RowItem = () => (
+    <ListItem
+      itemDivider
+      style={{
+        backgroundColor: 'white',
+        marginRight: spacing.screenPadding,
+        borderRadius: 15,
+        padding: 16,
+      }}
+    >
+      <View
+        style={{
+          padding: 4,
+          width: 50,
+          height: 50,
+          borderRadius: 15,
+          backgroundColor: color.backgroundColor,
+        }}
+      >
+        <Thumbnail
+          square
+          style={{
+            width: 42,
+            height: 42,
+            resizeMode: 'contain',
+          }}
+          source={{ uri: 'https://www.arla.dk/UI/img/arla-logo@2x.02d13ae2.png' }}
+        />
+      </View>
+      <View style={{ paddingLeft: 16 }}>
+        <Text style={{ fontFamily: 'Gilroy' }}>Arla</Text>
+        <Text style={{ fontFamily: 'Gilroy', color: 'grey' }}>Produktionsarbejde</Text>
+      </View>
+      <View style={{ flex: 1, alignItems: 'flex-end' }}>
+        <Text>dkk 50/t</Text>
+      </View>
+      <Right>
+        <Icon name="arrow-forward" />
+      </Right>
+    </ListItem>
+  );
+
   return (
     <Container>
       <List style={{ paddingLeft: spacing.screenPadding }}>
-        <ListItem
-          itemHeader
-          first
-          itemDivider
-          style={{
-            paddingBottom: spacing.interPadding,
-            paddingTop: spacing.interPadding,
-            paddingLeft: 0,
-            backgroundColor: 'transparent',
-          }}
-        >
-          <Text style={{ fontFamily: 'Gilroy', fontSize: 17 }}>Close to you</Text>
-        </ListItem>
+        <ItemDivider title="Close to you" />
         <FlatList horizontal renderItem={renderItem} data={['', '']} />
+        <ItemDivider title="Recently added" />
+        <RowItem />
       </List>
     </Container>
   );
